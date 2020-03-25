d6d8c375d2914e6feb3be0e708dbfafd7d7bb415
alaskast0rm <montanaof300@mail.ru>
2020-03-25 05:17:53 +0300

    1

--- bot.py
+++ bot.py
@@ -10,7 +10,12 @@ import datetime
 import re
 import csv
 import numpy as np
+import pandas as pd
+import matplotlib as mpl
+import seaborn as sns
 import matplotlib.pyplot as plt
+import warnings; warnings.filterwarnings(action='once')
+
 
 
 
@@ -31,46 +36,125 @@ def message_handler(bot: Bot, update: Update):
 	text = update.effective_message.text
 	reply_text = f'Ну здарова {name}\n\n{text}'
 
-	if 'gra' in text:
-		rng = np.arange(50)
-		rnd = np.random.randint(0, 10, size=(3, rng.size))
-		yrs = 1950 + rng
+	if 'ga' in text:
+		large = 22
+		med = 16
+		small = 12
+		params = {'axes.titlesize': large,
+				  'legend.fontsize': med,
+				  'figure.figsize': (16, 10),
+				  'axes.labelsize': med,
+				  'axes.titlesize': med,
+				  'xtick.labelsize': med,
+				  'ytick.labelsize': med,
+				  'figure.titlesize': large}
+		plt.rcParams.update(params)
+		plt.style.use('seaborn-whitegrid')
+		sns.set_style("white")
 
-		fig, ax = plt.subplots(figsize=(5, 3))
-		ax.stackplot(yrs, rng + rnd, labels=['Eastasia', 'Eurasia', 'Oceania'])
-		ax.set_title('Combined debt growth over time')
-		ax.legend(loc='upper left')
-		ax.set_ylabel('Total debt')
-		ax.set_xlim(xmin=yrs[0], xmax=yrs[-1])
-		fig.tight_layout()
-		a = fig.savefig('www.png')
+		# Version
+		print(mpl.__version__)  # > 3.0.0
+		print(sns.__version__)  # > 0.9.0
 
-		#a = plt.show()
-		print('a')
-		photo = open("www.png", "rb")
+		# rng = np.arange(50)
+		# rnd = np.random.randint(0, 10, size=(3, rng.size))
+		# yrs = 2000 + rng
+		#
+		# fig, ax = plt.subplots(figsize=(5, 3))
+		# ax.stackplot(yrs, rng + rnd, labels=['Eastasia', 'Eurasia', 'Oceania'])
+		# ax.set_title('Combined debt growth over time')
+		# ax.legend(loc='upper left')
+		# ax.set_ylabel('Total debt')
+		# ax.set_xlim(xmin=yrs[0], xmax=yrs[-1])
+		# fig.tight_layout()
+		# x = np.arange(0, 5, 0.1)
+		# y = x*2 + 2 - 20 + (1 / 120)
+		# plt.figure(dpi=200)
+		# plt.plot(x, y)
+		# plt.xlabel('Сашенька хуй саси')
+		# plt.ylabel('хихихихи')
+		# plt.savefig('www.png')
+		# #a = plt.show()
+		# print('a')
+		# photo = open("www.png", "rb")
+		# Import Data
+		df = pd.read_csv('dataa.csv')
+
+		print(df)
+
+		# Draw Plot
+		plt.figure(figsize=(16, 10), dpi=80)
+		print('figure')
+		plt.plot('date', 'low', data=df, color='red')
+
+		print('Draw')
+
+		# Decoration
+		plt.ylim(50, 750)
+		xtick_location = df.index.tolist()[::366]
+		xtick_labels = [x[:4] for x in df.date.tolist()[::366]]
+		plt.xticks(ticks=xtick_location, labels=xtick_labels, rotation=0, fontsize=12, horizontalalignment='center',
+				   alpha=.7)
+		plt.yticks(fontsize=12, alpha=.7)
+		plt.title("Air Passengers Traffic (1949 - 1969)", fontsize=22)
+		plt.grid(axis='both', alpha=.3)
+
+		print('Decoration')
+
+		# Remove borders
+		plt.gca().spines["top"].set_alpha(0.0)
+		print('top')
+		plt.gca().spines["bottom"].set_alpha(0.3)
+		print('bottom')
+		plt.gca().spines["right"].set_alpha(0.0)
+		print('right')
+		plt.gca().spines["left"].set_alpha(0.3)
+		print('left')
+		plt.savefig('ww4w.png')
+
+		print('Save')
+
+		photo = open('ww4w.png', 'rb')
+
+		print('Open')
 
 		bot.send_photo(
 			chat_id=update.effective_message.chat_id,
 			photo=photo,
 
 		)
-
-
+		print("Send")
 
 
 	if '/dol' in text:
-		headers = head
-		url = 'https://www.banki.ru/products/currency/usd/'
+		url = 'http://www.profinance.ru/currency_usd.asp'
 		sesion = requests.Session()
-		request = sesion.get(url, headers=headers)
+		request = sesion.get(url, headers=head)
+
 		if request.status_code == 200:
 			soup = bs(request.content, 'html.parser')
-			divs = soup.find_all('div', attrs={'class': 'layout-columns-wrapper'})
 
-			for div in divs:
-				divka = div.find('div', attrs={'class': "currency-table__large-text"}).text
-				divka1 = divka + 'RUB за 1$'
-				reply_text = divka1
+			td_cb = soup.find_all('td', attrs={'align': "center", 'class': 'cell'})
+
+			name_cb = td_cb[0].text
+			from_date_cb = td_cb[1].text
+			on_date_cb = td_cb[2].text
+			value_from_date_cb = td_cb[5].text
+			value_on_date_cb = td_cb[6].text
+			name_forex = td_cb[-6].text
+			date_forex = td_cb[-5].text
+			name_moscow = td_cb[-4].text
+			date_moscow = td_cb[-3].text
+			value_forex = td_cb[-2].text
+			value_moscow = td_cb[-1].text
+			print(value_moscow)
+
+			reply_text = name_cb + from_date_cb + value_from_date_cb + on_date_cb + value_on_date_cb + name_forex + '\n' + date_forex + '\n' + value_forex + '\n' + name_moscow + '\n' + date_moscow + '\n' + value_moscow
+			print(reply_text)
+
+		else:
+			reply_text = "Ошибка с подклюючением."
+
 		bot.send_message(
 			chat_id=update.effective_message.chat_id,
 			text=reply_text,
@@ -103,10 +187,13 @@ def message_handler(bot: Bot, update: Update):
 				response += f'{counter}) ' + str(result(divka)) + f"\n{result_https(mod_divka_https)}\n"
 			reply_text = response
 
-			bot.send_message(
-				chat_id=update.effective_message.chat_id,
-				text=reply_text,
-			)
+		else:
+			reply_text = "Ошибка с подклюючением."
+
+		bot.send_message(
+			chat_id=update.effective_message.chat_id,
+			text=reply_text,
+		)
 
 	if '/adhh' in text:
 		url = 'https://hh.ru/search/vacancy?only_with_salary=false&clusters=true&area=1&enable_snippets=true&salary=&st=searchVacancy&text=%D0%9C%D0%BB%D0%B0%D0%B4%D1%88%D0%B8%D0%B9+%D1%81%D0%B8%D1%81%D1%82%D0%B5%D0%BC%D0%BD%D1%8B%D0%B9+%D0%B0%D0%B4%D0%BC%D0%B8%D0%BD%D0%B8%D1%81%D1%82%D1%80%D0%B0%D1%82%D0%BE%D1%80&from=suggest_post'
@@ -134,10 +221,13 @@ def message_handler(bot: Bot, update: Update):
 				response += f'{counter}) ' + str(result(divka)) + f"\n{result_https(mod_divka_https).split('?')[0]}\n"
 			reply_text = response
 
-			bot.send_message(
-				chat_id=update.effective_message.chat_id,
-				text=reply_text,
-			)
+		else:
+			reply_text = "Ошибка с подклюючением."
+
+		bot.send_message(
+			chat_id=update.effective_message.chat_id,
+			text=reply_text,
+		)
 
 	global global_msg
 
@@ -158,8 +248,15 @@ def message_handler(bot: Bot, update: Update):
 			message_id=global_msg.message_id,
 		)
 
-	if '/seven' in text:
-		url = "https://studga.wohlnet.ru/d/full?fac=3&flow=188&grp=2&lsubgrp=3&esubgrp=1"
+	if '/veven' in text:
+		print('veven')
+
+		bot.send_message(
+			chat_id=update.effective_message.chat_id,
+			text="Пожалуйста, подождите пару секунд.",
+		)
+
+		url = "1https://studga.wohlnet.ru/d/full?fac=3&flow=188&grp=2&lsubgrp=3&esubgrp=1"
 		headers = head
 		session = requests.Session()
 		request = session.get(url, headers=headers)
@@ -190,23 +287,30 @@ def message_handler(bot: Bot, update: Update):
 					chat_id=update.effective_message.chat_id,
 					text=text
 				)
-		elif text == '/seven@scarlet_effect_bot' or text == '/seven':
-				if request.status_code == 200:
-					soup = bs(request.content, 'html.parser')
-					try:
-						body_today = soup.find('tr', attrs={'style': "height: 60px;", 'class': "tr_today"})
-						date = body_today.find('a')['href']
-					except AttributeError:
-						body_tomorrow = soup.find('tr', attrs={'style': "height: 60px;", 'class': "tr_tomorrow"})
-						date = body_tomorrow.find('a')['href']
-					new_date = date.split('=')[-1]
-					day = new_date[8:]
-					month = int(new_date[5:7])
-					year = int(new_date[:4])
-					if day[0] == 0:
-						day = int(day[1])
-					else:
-						day = int(day)
+		elif text == '/veven@scarlet_effect_bot' or text == '/veven':
+			if request.status_code == 200:
+				soup = bs(request.content, 'html.parser')
+				try:
+					body_today = soup.find('tr', attrs={'style': "height: 60px;", 'class': "tr_today"})
+					date = body_today.find('a')['href']
+				except AttributeError:
+					body_tomorrow = soup.find('tr', attrs={'style': "height: 60px;", 'class': "tr_tomorrow"})
+					date = body_tomorrow.find('a')['href']
+				new_date = date.split('=')[-1]
+				day = new_date[8:]
+				month = int(new_date[5:7])
+				year = int(new_date[:4])
+				if day[0] == 0:
+					day = int(day[1])
+				else:
+					day = int(day)
+
+			else:
+				bot.send_message(
+					chat_id=update.effective_message.chat_id,
+					text="Ошибка с подключением."
+				)
+
 		else:
 			bot.send_message(
 				chat_id=update.effective_message.chat_id,
@@ -220,45 +324,55 @@ def message_handler(bot: Bot, update: Update):
 			if len(str(month)) == 1:
 				month = '0' + str(month)
 
-			url = f'https://studga.wohlnet.ru/d/oneday?fac=3&flow=188&grp=2&lsubgrp=3&esubgrp=1&ofdate={year}-{month}-{day}'
-			session = requests.Session()
-			request = session.get(url, headers=head)
+			try:
 
-			if request.status_code == 200:
-				soup = bs(request.content, 'html.parser')
-				tbody = soup.find_all('tr', {'style': "background-color: #FFFFFF; "})
-				counter = 1
-				output = ''
-				output_table_day_of_the_week = soup.find('center').find_all('b')[-1].text
-				date_and_day_of_the_week = '📅 ' + str(day) + '.' + str(month) + '.' + str(
-					year) + ' - ' + output_table_day_of_the_week + '\n'
+				url = f'https://studga.wohlnet.ru/d/oneday?fac=3&flow=188&grp=2&lsubgrp=3&esubgrp=1&ofdate={year}-{month}-{day}'
+				session = requests.Session()
+				request = session.get(url, headers=head)
 
-				for table in tbody:
-					output_table_para = table.find('td', {
-						'style': "width: 70px; border: 1px solid #000000; text-align: center;"}).find('strong').text
+				if request.status_code == 200:
 
-					output_table_time = table.find('td', {
-						'style': "width: 70px; border: 1px solid #000000; text-align: center;"}).find('small').text
+					soup = bs(request.content, 'html.parser')
+					tbody = soup.find_all('tr', {'style': "background-color: #FFFFFF; "})
+					counter = 1
+					output = ''
+					output_table_day_of_the_week = soup.find('center').find_all('b')[-1].text
+					date_and_day_of_the_week = '📅 ' + str(day) + '.' + str(month) + '.' + str(
+						year) + ' - ' + output_table_day_of_the_week + '\n'
 
-					output_table_subject = table.find('td', {'style': "border: 1px solid #000000"}).find('b').text
+					for table in tbody:
+						output_table_para = table.find('td', {
+							'style': "width: 70px; border: 1px solid #000000; text-align: center;"}).find('strong').text
 
-					output_table_teacher = table.find('td', {'style': "border: 1px solid #000000"}).find(
-						'small').text
+						output_table_time = table.find('td', {
+							'style': "width: 70px; border: 1px solid #000000; text-align: center;"}).find('small').text
 
-					output_table_aud = table.find('td', {'style': "border: 1px solid #000000"}).find('i').text
+						output_table_subject = table.find('td', {'style': "border: 1px solid #000000"}).find('b').text
 
-					output_table_kind = soup.find_all('i')[counter].text
+						output_table_teacher = table.find('td', {'style': "border: 1px solid #000000"}).find(
+							'small').text
 
-					counter += 2
+						output_table_aud = table.find('td', {'style': "border: 1px solid #000000"}).find('i').text
 
-					output += '\n◽️ ' + output_table_para + '\n🕙 ' + output_table_time + \
-							  '\n📖 ' + output_table_subject + '\n👤 ' + output_table_teacher \
-							  + '\n🏢 ' + output_table_aud + '\n⚪️ ' + output_table_kind + '\n\n'
-				if len(output) == 0:
-					output = 'Пар нет, отдыхаем '
+						output_table_kind = soup.find_all('i')[counter].text
 
-				reply_text = date_and_day_of_the_week + output
-				return reply_text
+						counter += 2
+
+						output += '\n◽️ ' + output_table_para + '\n🕙 ' + output_table_time + \
+								  '\n📖 ' + output_table_subject + '\n👤 ' + output_table_teacher \
+								  + '\n🏢 ' + output_table_aud + '\n⚪️ ' + output_table_kind + '\n\n'
+					if len(output) == 0:
+						output = 'Пар нет, отдыхаем '
+
+					reply_text = date_and_day_of_the_week + output
+					return reply_text
+
+			except:
+				bot.send_message(
+					chat_id=update.effective_message.chat_id,
+					text="Ошибка с подключением."
+				)
+
 		if day > 31 or month > 12:
 			bot.send_message(
 				chat_id=update.effective_message.chat_id,
@@ -545,14 +659,14 @@ def message_handler(bot: Bot, update: Update):
 						if "Имеется новое расписание, занятия ещё не начались" in reserve_responce:
 							reply_text = "❌ Неправильный ввод !"
 				else:
-					reply_text = "A error connection"
+					reply_text = "Ошибка с подклюючением."
 			else:
 				reply_text = "❌ Неправильный ввод !"
 
 		else:
 			reply_text = "❌ Неправильный ввод !"
 
-		if reply_text == "❌ Неправильный ввод !" or reply_text == "A error connection":
+		if reply_text == "❌ Неправильный ввод !" or reply_text == "Ошибка с подключением.":
 			bot.send_message(
 				chat_id=update.effective_message.chat_id,
 				text=reply_text + '\nВремя на сервере: ' + now_time,
@@ -633,10 +747,13 @@ def message_handler(bot: Bot, update: Update):
 				temperature_feels(divs_feels)) + str(divs_condition) + emoji + str(yesterday_temperature()) + str(moscow_time)
 			reply_text = response
 
-			bot.send_message(
-				chat_id=update.effective_message.chat_id,
-				text=reply_text,
-			)
+		else:
+			reply_text = "Ошибка с подклюючением."
+
+		bot.send_message(
+			chat_id=update.effective_message.chat_id,
+			text=reply_text,
+		)
 
 
 def main():
